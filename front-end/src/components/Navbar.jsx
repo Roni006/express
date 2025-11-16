15ca646c0339cccef5f2c562b224cb463da289de
Anik Islam <anik54908@gmail.com>
2025-11-16 12:51:48 +0600

    crated navbar

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,5 +1,8 @@
 import { Link } from 'react-router'
 import logo from '../assets/logo.png'
+import user from '../assets/user.svg';
+import cart from '../assets/cart.svg';
+import { CgMenuRight } from "react-icons/cg"; 
 const Navbar = () => {
     return (
         <>
@@ -11,16 +14,24 @@ const Navbar = () => {
                                 <img src={logo} alt="header logo" />
                             </Link>
                         </div>
-                        <div className="menus">
+                        <div className="menus hidden md:block sm:block">
                             <nav>
-                                <ul className='menu  lg:flex items-center gap-[50px] hidden sm:block md:block  '>
+                                <ul className='menu flex items-center gap-[50px] '>
                                     <li><Link to="/">Discovery</Link></li>
                                     <li><Link to="/about">About</Link></li>
                                     <li><Link to="/contact">Conatat Us</Link></li>
                                 </ul>
                             </nav>
                         </div>
-                        <div className="icons"></div>
+                        <div className="right-icons md:block hidden ">
+                            <div className="icon flex gap-[15px] cursor-pointer">
+                                <img src={user} alt="User icon" />
+                                <img src={cart} alt="Cart icon" />
+                            </div>
+                        </div>
+                        <div className="hamburger bg-[#53cd8a] text-[30px] px-2 py-2 text-white cursor-pointer rounded-md md:hidden block">
+                            <CgMenuRight />
+                        </div>
                     </div>
                 </div>
             </header>
